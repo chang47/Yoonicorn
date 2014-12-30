235ef3198aa69213fa7356fabb1ccdb4fac1eb6f
yoonj4 <justin.o.yoon@gmail.com>
2014-12-30 00:08:23 -0800

    Edited scene so unit was not standing on top of a rock. Figured out movement range calculation and how to draw the movement range.

--- LiberationDay/Assets/Scripts/MegaController.cs
+++ LiberationDay/Assets/Scripts/MegaController.cs
@@ -14,7 +14,14 @@ public class MegaController : MonoBehaviour {
 	void Start () {
 		//LevelManager_Script.val = 123213;
 		//Debug.Log (LevelManager_Script.val);
-		LevelManager_Script.units [(int)transform.position.x, (int)-transform.position.y] = true;
+		LevelManager_Script.units [(int)-transform.position.y, (int)transform.position.x] = true;
+		/*for (int i = 0; i < LevelManager_Script.rows; i++) {
+			for (int j = 0; j < LevelManager_Script.columns; j++) {
+				if (LevelManager_Script.units [i, j]) {
+					Debug.Log ("i: " + i + "\nj: " + j);
+				}
+			}
+		}*/
 		movementRange = new HashSet<GameObject>();
 	}
 	
@@ -37,7 +44,8 @@ public class MegaController : MonoBehaviour {
 	void draw(int aID) {
 		if(GUI.Button (new Rect (0,20,100,50), "Move")){
 			//transform.position = new Vector2(transform.position.x + 1, transform.position.y + 1);
-
+			drawMovementRange();
+			//Debug.Log("movementRange: " + movementRange.Count);
 			showMenu = false;
 		}else if(GUI.Button (new Rect (0,70,100,50), "Attack")){
 			//Options code goes here
@@ -50,46 +58,60 @@ public class MegaController : MonoBehaviour {
 		Debug.Log(LevelManager_Script.units[(int)transform.position.x, (int)-transform.position.y]);
 		showMenu = !showMenu;
 		if(showMenu) {
-			Debug.Log("true");
+			//Debug.Log("true");
 		} else {
-			Debug.Log("false");
+			//Debug.Log("false");
 		}
 	}
 
 	void drawMovementRange() {
-				// TODO: figure out NOWNOWNOW.
+		calculateMovementRange ();
+		foreach (GameObject tile in movementRange) {
+			tile.transform.GetChild(0).renderer.material.color = Color.green;
+		}
 	}
 
 	void calculateMovementRange() {
 		Stack<GameObject> s = new Stack<GameObject> ();
 		int unitX = (int)transform.position.x;
-		int unitY = (int)transform.position.y;
-		s.Push (LevelManager_Script.terrain [unitX, unitY]);
+		int unitY = (int)-transform.position.y;
+		s.Push (LevelManager_Script.terrain [unitY, unitX]);
 		while (s.Count > 0) {
 			GameObject current = s.Pop();
 			int currentX = (int) current.transform.position.x;
 			int currentY = (int) -current.transform.position.y;
 			int dx = Math.Abs(currentX - unitX);
 			int dy = Math.Abs(currentY - unitY);
+			int distance = dx + dy;
 
-			if (!movementRange.Contains(current) && dx + dy <= move && 
-			    (current == LevelManager_Script.terrain[unitX, unitY] || validTile(current))) {
+			//Debug.Log("unitX: " + unitX + "\nunitY: " + unitY + "\ncurrentX: " + currentX + "\ncurrentY: " + currentY);
+			//Debug.Log("distance: " + distance + "\nalready contained: " + movementRange.Contains(current) + "\nvalidTile: " + validTile(current));
+			if (!movementRange.Contains(current) && distance <= move && 
+			    (current == LevelManager_Script.terrain[unitY, unitX] || validTile(current))) {
 				movementRange.Add (current);
 				if (currentY > 0) {
-					s.Push(LevelManager_Script.terrain[currentX, currentY - 1]);
-				} else if (currentY < LevelManager_Script.rows - 1) {
-					s.Push(LevelManager_Script.terrain[currentX, currentY + 1]);
-				} else if (currentX > 0) {
-					s.Push (LevelManager_Script.terrain[currentX - 1, currentY]);
-				} else if (currentX < LevelManager_Script.columns - 1) {
-					s.Push(LevelManager_Script.terrain[currentX + 1, currentY]);
+					s.Push(LevelManager_Script.terrain[currentY - 1, currentX]);
+				}
+
+				if (currentY < LevelManager_Script.rows - 1) {
+					s.Push(LevelManager_Script.terrain[currentY + 1, currentX]);
+				}
+
+				if (currentX > 0) {
+					s.Push (LevelManager_Script.terrain[currentY, currentX - 1]);
+				}
+
+				if (currentX < LevelManager_Script.columns - 1) {
+					s.Push(LevelManager_Script.terrain[currentY, currentX + 1]);
 				}
 			}
 		}
 	}
 
 	bool validTile(GameObject tile) {
-		return !tile.GetComponent<TerrainTraits_Script>().blocks && 
-			!LevelManager_Script.units[(int)tile.transform.position.x, (int)-tile.transform.position.y];  
+		bool res = !tile.GetComponent<TerrainTraits_Script> ().blocks && 
+			!LevelManager_Script.units [(int)-tile.transform.position.y, (int)tile.transform.position.x];
+		//Debug.Log("validTile returned: " + res);
+		return res;  
 	}
 }
