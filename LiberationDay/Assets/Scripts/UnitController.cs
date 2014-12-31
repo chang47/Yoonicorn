e5a16a529cfe76f67fa2b311e078ebd461f4ed40
chang47 <coralbue@gmail.com>
2014-12-30 19:29:07 -0800

    real unit attack

---
+++ LiberationDay/Assets/Scripts/UnitController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+/*
+ * Battle system controller. Takes control of fighting and storing
+ * selected unit to allow battle and movement.
+ */
+public class UnitController : MonoBehaviour {
+
+	public static GameObject unit;
+	public static bool unitMove;
+	public static bool unitAttack;
+	public static bool inUse;
+
+	public static void battle(GameObject enemy) {
+		//receives enemy game object and fights
+		Debug.Log ();
+		Debug.Log ();
+	}
+
+	public static void move(GameObject terrain) {
+		//movement method
+	}
+
+}
